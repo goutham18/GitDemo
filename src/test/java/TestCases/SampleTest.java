8b4b7af97f35b0b26d771175de60023f2e42e4aa
karthik <gouthamr106@gmail.com>
2019-04-23 21:49:00 +0100

    first code

---
+++ src/test/java/TestCases/SampleTest.java
@@ -0,0 +1,20 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("method1");
+	}
+	
+
+	@Test
+	public void method2()
+	{
+		System.out.println("method2");
+	}
+
+}
---
+++ src/test/java/TestCases/SeleniumTest.java
@@ -0,0 +1,22 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+
+	@Test
+	public void testCase1()
+	{
+		System.out.println("testcase1");
+	}
+	
+
+	@Test
+	public void testCase2()
+	{
+		System.out.println("testcase2");
+	}
+
+
+}
